8b0888e51cb6452daa910a6a10e625ed63e54bb9
nitesh.dev <nitesh.dev@proton.me>
2026-03-02 22:22:11 +0530

    assignment-1 done without google search

---
+++ cpp/conditionals/assignments/assignment1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+    string fav_tea;
+    getline(cin, fav_tea);
+    if (fav_tea == "Oolong")
+    {
+        cout << "Have a great taste." << endl;
+    } 
+
+    // assignment1 completed
+    return 0;
+}
